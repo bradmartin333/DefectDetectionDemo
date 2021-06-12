fc646558e3d77079ee78e55938524670a988a5cf
BradDesktop <brad.martin@xdisplay.com>
2021-06-11 22:41:22 -0400

    update opencv, send frames from a video async and maintain control of form

---
+++ OpenCvDemo/Form1.cs
@@ -0,0 +1,80 @@
+﻿using Accord.Video.FFMPEG;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace OpenCvDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            GlobalMouseHandler.MouseMovedEvent += GlobalMouseHandler_MouseMovedEvent;
+            Application.AddMessageFilter(new GlobalMouseHandler());
+
+            InitializeComponent();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void GlobalMouseHandler_MouseMovedEvent(object sender, MouseEventArgs e)
+        {
+            label1.Text = string.Format("X: {0}, Y: {1}", e.X, e.Y);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            using (var vFReader = new VideoFileReader())
+            {
+                vFReader.Open("video.mp4");
+                for (int i = 0; i < vFReader.FrameCount; i++)
+                {
+                    Bitmap bmpBaseOriginal = vFReader.ReadVideoFrame();
+                    Bitmap resized = new Bitmap(bmpBaseOriginal, new System.Drawing.Size(bmpBaseOriginal.Width / 3, bmpBaseOriginal.Height / 3));
+                    Show(new Demo { Image = resized, Blur = 7, ThresholdLow = 10, ThresholdHigh = 30 });
+                }
+                vFReader.Close();
+            }
+        }
+
+        private void Show(Demo demo)
+        {
+            Mat img = demo.Image.ToMat();
+
+            #region blur
+            var blur = new Mat();
+            Cv2.GaussianBlur(img, blur, new OpenCvSharp.Size(demo.Blur, demo.Blur), 0);
+            #endregion
+
+            #region canny
+            var canny = new Mat();
+            Cv2.Canny(blur, canny, demo.ThresholdLow, demo.ThresholdHigh);
+            Cv2.Dilate(canny, canny, new Mat());
+            #endregion
+
+            #region contours
+            OpenCvSharp.Point[][] contours;
+            HierarchyIndex[] hierarchyIndexes;
+            Cv2.FindContours(
+                canny,
+                out contours,
+                out hierarchyIndexes,
+                mode: RetrievalModes.External,
+                method: ContourApproximationModes.ApproxSimple);
+            #endregion
+
+            if (demo.Filter) contours = contours.Where(x => x.Length > 50).ToArray();
+            Cv2.DrawContours(img, contours, -1, Scalar.Red, thickness: 1);
+
+            pictureBox1.Image = img.ToBitmap();
+            pictureBox1.Invoke(new MethodInvoker(Refresh));
+
+            Vec3b bgrPixel = img.Get<Vec3b>(0, 0);
+            pictureBox3.BackColor = Color.FromArgb(bgrPixel.Item0, bgrPixel.Item1, bgrPixel.Item2);
+            pictureBox3.Invoke(new MethodInvoker(Refresh));
+        }
+    }
+}
--- OpenCvDemo/Program.cs
+++ OpenCvDemo/Program.cs
@@ -1,69 +1,49 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using OpenCvSharp;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace OpenCvDemo
 {
     class Program
     {
-        static void Main()
+        private static void Main()
         {
-            var input = new List<Demo>
-            {
-                new Demo {File = "Dirty.jpg", Blur = 7, ThresholdLow = 10, ThresholdHigh = 30},
-                new Demo {File = "Oil.jpg", Blur = 11, ThresholdLow = 9, ThresholdHigh = 26, Filter = true},
-                new Demo {File = "Line.jpg", Blur = 11, ThresholdLow = 10, ThresholdHigh = 25, Filter = true}
-            };
-
-            Show(input[0]);
-        }
-
-        public static void Show(Demo demo)
-        {
-            var org = new Mat();
-            var img = Cv2.ImRead(demo.File);
-            var gray = new Mat(demo.File, ImreadModes.GrayScale);
-            img.CopyTo(org);
-
-            #region blur
-            var blur = new Mat();
-            Cv2.GaussianBlur(gray, blur, new Size(demo.Blur, demo.Blur), 0);
-            #endregion
-
-            #region canny
-            var canny = new Mat();
-            Cv2.Canny(blur, canny, demo.ThresholdLow, demo.ThresholdHigh);
-            Cv2.Dilate(canny, canny, new Mat());
-            #endregion
-
-            #region contours
-            Point[][] contours;
-            HierarchyIndex[] hierarchyIndexes;
-            Cv2.FindContours(
-                canny,
-                out contours,
-                out hierarchyIndexes,
-                mode: RetrievalModes.External,
-                method: ContourApproximationModes.ApproxSimple);
-            #endregion
-
-            if(demo.Filter) contours = contours.Where(x => x.Length > 40).ToArray();
-            Cv2.DrawContours(img, contours, -1, Scalar.Red, thickness: 2);
-
-            using (new Window("image", img))
-            using (new Window("org", org))
-            {
-                Cv2.WaitKey();
-            }
+            Application.Run(new Form1());
         }
     }
 
     public class Demo
     {
-        public string File { get; set; }
+        public Bitmap Image { get; set; }
         public int Blur { get; set; }
         public int ThresholdLow { get; set; }
         public int ThresholdHigh { get; set; }
         public bool Filter { get; set; }
+    }
+
+    public class GlobalMouseHandler : IMessageFilter
+    {
+        private const int WM_MOUSEMOVE = 0x0200;
+        private System.Drawing.Point previousMousePosition = new System.Drawing.Point();
+        public static event EventHandler<MouseEventArgs> MouseMovedEvent = delegate { };
+
+        #region IMessageFilter Members
+
+        public bool PreFilterMessage(ref System.Windows.Forms.Message m)
+        {
+            if (m.Msg == WM_MOUSEMOVE)
+            {
+                System.Drawing.Point currentMousePoint = Control.MousePosition;
+                if (previousMousePosition != currentMousePoint)
+                {
+                    previousMousePosition = currentMousePoint;
+                    MouseMovedEvent(this, new MouseEventArgs(MouseButtons.None, 0, currentMousePoint.X, currentMousePoint.Y, 0));
+                }
+            }
+            // Always allow message to continue to the next filter control
+            return false;
+        }
+
+        #endregion
     }
 }
