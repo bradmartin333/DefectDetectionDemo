0613deb4da3baf9455df7cb575ccc4f8bb3b1ae1
BradDesktop <brad.martin@xdisplay.com>
2021-06-11 22:46:06 -0400

    add git ignore

--- OpenCvDemo/Form1.cs
+++ OpenCvDemo/Form1.cs
@@ -29,7 +29,7 @@ namespace OpenCvDemo
         {
             using (var vFReader = new VideoFileReader())
             {
-                vFReader.Open("video.mp4");
+                vFReader.Open(@"C:\Users\delta\Downloads\video.mp4");
                 for (int i = 0; i < vFReader.FrameCount; i++)
                 {
                     Bitmap bmpBaseOriginal = vFReader.ReadVideoFrame();
