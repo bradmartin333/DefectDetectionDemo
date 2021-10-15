b942b3130dae859817975cc768fcc12ed42d379d
BradLaptop <brad.martin@xdisplay.com>
2021-10-15 12:25:24 -0400

    change proj name

--- PostFinder/Properties/Resources.Designer.cs
+++ PostFinder/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace OpenCvDemo.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap RunSPOT {
-            get {
-                object obj = ResourceManager.GetObject("RunSPOT", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
